fcec6345a88b30dffd404db26e11a4bd8afa1c28
Huajian Chiu <huajian.qiu@epfl.ch>
2020-01-03 02:03:42 +0100

    ex3 dist works, without adding diagnostics

--- Project_ex3.py
+++ Project_ex3.py
@@ -1,6 +1,10 @@
 import numpy as np
 import scipy.stats as st
 import matplotlib.pyplot as plt
+from scipy.special import logsumexp
+import matplotlib.colors as mcolors
+import warnings
+warnings.simplefilter("ignore")
 
 
 def metropolis_hastings(x, p, u):
@@ -51,46 +55,59 @@ def simple_parallel_tempering(x, K, N, p, u, u0, Ns):
     return x[0]
 
 
+a = np.array([-2, -1])
+b = np.array([13, 3])
+# np.random.seed(np.random.randint(1)+comm.rank)
+signoise = np.array([0.05])
+
+
+# Defines likelihood, prior and posterior functions
+def L(x):
+    # C=\left(x-1.7\right)^2+\left(y-1\right)^2=1\left\{x\le1.8+.7\right\}
+    m = (1.01 / (3.142 - 4.9))
+    C = -0.5 * (((x[0] - 1.7) ** 2 + (x[1] - 1) ** 2 - 1.0) ** 2) / (signoise ** 2) + np.log((x[0] <= 2.5))
+    I1 = -0.5 * ((x[1] - 0) ** 2) / (signoise ** 2) + np.log(x[0] < 11) + np.log(x[0] >= 9)
+    I2 = -0.5 * ((x[1] - 2) ** 2) / (signoise ** 2) + np.log(x[0] < 11) + np.log(x[0] >= 9) + np.log(
+        x[1] >= 0) + np.log(x[1] < 2)
+    I3 = -0.5 * ((x[0] - 10) ** 2) / (signoise ** 2) + np.log(x[1] < 2) + np.log(x[0] >= 0)
+    Q1 = -0.5 * (((x[0] - 7) ** 2 + (x[1] - 1) ** 2 - 1.0) ** 2) / (signoise ** 2)
+    Q2 = -0.5 * ((x[1] + x[0] - 8) ** 2) / (signoise ** 2) + np.log(x[0] < 8.4) + np.log(x[0] > 7.7)
+
+    S1 = -0.5 * ((x[1] - (m * x[0] + (1.5 - m * 3.142))) ** 2) / (signoise ** 2) + np.log(x[0] < 4.86) + np.log(
+        x[0] > 3.142)
+
+    S2 = -0.5 * (((x[0] - 4) ** 2 + (x[1] - 1) ** 2 - 1) ** 2) / (signoise ** 2) + np.log(x[1] < 0.5)
+    S3 = -0.5 * (((x[0] - 4) ** 2 + (x[1] - 1) ** 2 - 1) ** 2) / (signoise ** 2) + np.log(x[1] > 1.5)
+    return logsumexp([C, I1, I2, I3, Q1, Q2, S1, S2, S3]);
+
+
+def pr(x):
+    return np.log((x[0] >= a[0]) * (x[0] <= b[0]) * (x[1] >= a[1]) * (x[1] <= b[1]));
+
+
+def post(x):
+    return L(x) + pr(x)
+
+
 acc = 0
 K = 4
-a = 2
-N = 100
+Ta = 2
+N = 10000
 Ns = 1
-sigma = 0.05
-phi1 = lambda x, y: ((x - 1.7) ** 2 + (y - 1) ** 2 - 1) ** 2 * (x <= 2.5) / (-2 * sigma ** 2)
-phi2 = lambda x, y: y ** 2 * (x >= 9) * (x < 11) / (-2 * sigma ** 2)
-phi3 = lambda x, y: (y - 2) ** 2 * (x >= 9) * (x < 11) * (y >= 0) * (y < 2) / (-2 * sigma ** 2)
-phi4 = lambda x, y: ((x - 7) ** 2 + (y - 1) ** 2 - 1) ** 2 / (-2 * sigma ** 2)
-phi5 = lambda x, y: (y + x - 8) ** 2 * (x > 7.7) * (x < 8.4) / (-2 * sigma ** 2)
-phi6 = lambda x, y: (y - (-0.57 * x + 3.25)) ** 2 * (x > 3.142) * (x < 4.86) / (-2 * sigma ** 2)
-phi7 = lambda x, y: ((x - 4) ** 2 + (y - 1) ** 2 - 1) ** 2 * ((y < 0.5) + (y > 1.5)) / (-2 * sigma ** 2)
-
-u = [lambda theta, i=i: np.exp(-np.log(
-    np.exp(phi1(theta[0], theta[1])) + np.exp(phi2(theta[0], theta[1])) + np.exp(phi3(theta[0], theta[1])) + np.exp(phi4(theta[0], theta[1])) +
-    np.exp(phi5(theta[0], theta[1])) + np.exp(phi6(theta[0], theta[1])) + np.exp(phi7(theta[0], theta[1]))
-) / (a ** i)) * (theta[0] <= 13) * (theta[0] >= -2) * (theta[1] >= -1) * (theta[1] <= 3) for i in range(4)]
-
-u1 = [lambda theta, i=i: np.power(
-    np.exp(-phi1(theta[0], theta[1])) + np.exp(-phi2(theta[0], theta[1])) + np.exp(-phi3(theta[0], theta[1])) + np.exp(-phi4(theta[0], theta[1])) +
-    np.exp(-phi5(theta[0], theta[1])) + np.exp(-phi6(theta[0], theta[1])) + np.exp(-phi7(theta[0], theta[1])),
-    -1 / (a ** i)) * ((theta[0] <= 13) * (theta[0] >= -2) * (theta[1] >= -1) * (theta[1] <= 3)) for i in range(4)]
-
-
-p = [lambda loc: st.multivariate_normal.rvs(mean=loc, cov=np.array([[4, 0], [0, 1]]))] * K
+u = [lambda x, i=i: np.exp(post(x) / Ta ** i) for i in range(4)]
+p = [lambda loc: st.multivariate_normal.rvs(mean=loc, cov=np.array([[1, 0], [0, 0.5]]))] * K
 u0 = st.uniform(loc=np.array([-2, -1]), scale=np.array([15, 4])).rvs
 x = np.zeros((K, N, 2))
-theta0 = np.arange(-2, 13, 0.1)
-theta1 = np.arange(-1, 3, 0.1)
-Theta0, Theta1 = np.meshgrid(theta0, theta1)
-Theta_positions = np.vstack([Theta0.ravel(), Theta1.ravel()])
-prob_f = u1[0](Theta_positions)
-plt.imshow(prob_f.reshape((150, 40)), interpolation='none')
-plt.show()
-'''
-xs = simple_parallel_tempering(x, K, N, p, us, u0, Ns)
+
+x0 = simple_parallel_tempering(x, K, N, p, u, u0, Ns)
 stat = {'acceptance rate': acc / ((N - 1) * K)}
 print('acceptance rate: %f' % (stat['acceptance rate']))
-fig = plt.figure(figsize=(4, 4))
-plt.hist2d(xs[0], xs[1], bins=30)
+xmin = -2
+xmax = 13
+ymin = -1
+ymax = 3
+fig = plt.figure(figsize=(15, 4))
+plt.hist2d(x0[:, 0], x0[:, 1], bins=[150, 40], norm=mcolors.PowerNorm(0.3), range=[[xmin, xmax], [ymin, ymax]])
+plt.savefig('figures/csqi_hist2d.png')
 plt.show()
-'''
+
